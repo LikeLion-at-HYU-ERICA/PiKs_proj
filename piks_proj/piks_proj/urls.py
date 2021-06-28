fd2eeabe010308eb21d7136389f31762585ccf02
cho <ciccic11@naver.com>
2021-06-28 22:19:24 +0900

    cho

--- piks_proj/piks_proj/urls.py
+++ piks_proj/piks_proj/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import piks_app.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', piks_app.views.home, name='home'),
 ]
